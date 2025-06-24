a31021213fbb0da3c1dbcba8324ee52c1a0575c9
PabloAcuaviva <pablohacuaviva@gmail.com>
2025-06-24 14:53:09 +0200

    Added imports

--- visual_logic/tasks/__init__.py
+++ visual_logic/tasks/__init__.py
@@ -1,9 +1,1 @@
-from . import (
-    base,
-    game_of_life,
-    json_arc,
-    langton_ant,
-    maze,
-    problem_set,
-    typing_and_extensions,
-)
+from . import base, game_of_life, json_arc, langton_ant, maze, problem_set
