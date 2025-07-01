bee5e573d0e887361cdd8984edc8a092b2faf35c
PabloAcuaviva <pablohacuaviva@gmail.com>
2025-07-01 20:10:25 +0200

    Added navigation2D

--- generate.py
+++ generate.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     from visual_logic.tasks.game_of_life import GameOfLife
     from visual_logic.tasks.langton_ant import LangtonAnt
     from visual_logic.tasks.maze import Maze
+    from visual_logic.tasks.navigation2d import Navigation2D
     from visual_logic.tasks.problem_set import TaskProblemSet
     from visual_logic.tasks.render.schemas import ArcBaseStyle, MazeBaseStyle
 
@@ -103,14 +104,68 @@ if __name__ == "__main__":
             "datasets/langton_ant/test", ArcBaseStyle
         )
 
+    def generate_navigation2d_dataset(
+        generate_intermediate_states: bool = False,
+        subset_sizes: Optional[list[int]] = None,
+        n_train: int = 1000,
+        n_test: int = 100,
+    ):
+        def path_distance(
+            task_problem_0: TaskProblem,
+            task_problem_1: TaskProblem,
+        ) -> float:
+            path0 = set(task_problem_0.task_specific_metadata["path"])
+            path1 = set(task_problem_1.task_specific_metadata["path"])
+            if len(path0) == 0 and len(path1) == 0:
+                raise ValueError("Both paths are empty")
+            intersection = len(path0.intersection(path1))
+            union = len(path0.union(path1))
+            jaccard_similarity = intersection / union
+            return 1.0 - jaccard_similarity
+
+        train_dataset, test_dataset = TaskDatasetGenerator(
+            task=Navigation2D(
+                width=21,
+                height=21,
+                obstacle_density=0.08,
+                seed=42,
+                add_border=True,
+                valid_starts=(1, 1),
+                valid_ends=(19, 19),
+                n_barriers=8,
+                barrier_holes_range=(3, 5),
+                n_blocks=(1, 4),
+                block_size_range=(2, 3),
+                generate_intermediate_states=generate_intermediate_states,
+                max_attempts=4000,
+            ),
+            dist_fn=path_distance,
+        ).generate(n_train=n_train, n_test=n_test, distance_threshold=0.5)
+
+        shutil.rmtree("datasets/navigation2d", ignore_errors=True)
+        TaskProblemSet(task_problems=train_dataset).save(
+            "datasets/navigation2d/train",
+            MazeBaseStyle,
+            subset_sizes=subset_sizes,
+        )
+        TaskProblemSet(task_problems=test_dataset).save(
+            "datasets/navigation2d/test", MazeBaseStyle
+        )
+
     ###
     # Datasets generation for different tasks
     ###
-    generate_maze_dataset(
+    # generate_maze_dataset(
+    #     generate_intermediate_states=False,
+    #     subset_sizes=[1, 3, 5, 10, 20, 40, 60, 80, 100, 200, 500, 1000],
+    #     n_train=1000,
+    #     n_test=100,
+    # )
+    # generate_gol_dataset()
+    # generate_langton_ant_dataset()
+    generate_navigation2d_dataset(
         generate_intermediate_states=False,
         subset_sizes=[1, 3, 5, 10, 20, 40, 60, 80, 100, 200, 500, 1000],
         n_train=1000,
         n_test=100,
     )
-    # generate_gol_dataset()
-    # generate_langton_ant_dataset()
